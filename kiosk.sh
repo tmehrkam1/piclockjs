44da69836b6efe37020c00cb44069c2b4b0748da
tom mehrkam <tmehrkam@gmail.com>
2018-07-03 18:36:01 -0400

    Added install notes from a fresh pi build.  Probably need to doc this better.

---
+++ kiosk.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+export DISPLAY=:0.0
+unclutter &
+
+# If Chrome crashes (usually due to rebooting), clear the crash flag so we don't have the annoying warning bar
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+
+/usr/bin/chromium-browser --kiosk --disable-infobars --disable-extensions http://localhost:8081 &
