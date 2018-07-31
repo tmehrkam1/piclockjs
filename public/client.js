4990ffec984579c759230a70caec77d396fa9af2
tom mehrkam <tmehrkam@gmail.com>
2018-07-31 18:39:14 -0400

    single / double quotes

--- public/client.js
+++ public/client.js
@@ -226,11 +226,11 @@ function updateCur() {
 		});
 		
 		if (data.pressureTrend == 1 || data.pressureTrend == null) {
-			presTrendIcon = "<i class="fas fa-circle"></i>";
+			presTrendIcon = '<i class="fas fa-circle"></i>';
 		} else if (data.pressureTrend < 1) {
-			presTrendIcon = "<i class="fas fa-chevron-circle-down"></i>";
+			presTrendIcon = '<i class="fas fa-chevron-circle-down"></i>';
 		} else {
-			presTrendIcon = "<i class="fas fa-chevron-circle-up"></i>";
+			presTrendIcon = '<i class="fas fa-chevron-circle-up"></i>';
 		}
 		document.getElementById("curIcon").src = data.curIcon;
 		document.getElementById("curHum").innerHTML = 'Humidity : ' + data.humidity + ' %';
