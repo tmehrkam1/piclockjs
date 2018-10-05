5ebdd2abd6c6fa4b27d948ff5b65107014d62829
tom mehrkam <tmehrkam@gmail.com>
2018-10-05 15:45:33 -0400

    need to blank out hand ticks

--- public/clock.js
+++ public/clock.js
@@ -6,28 +6,13 @@ radius = radius * 0.90
 setInterval(drawClock, 1000);
 
 function drawClock() {
-  //drawFace(ctx, radius);
+  drawFace(ctx, radius);
   drawNumbers(ctx, radius);
   drawTime(ctx, radius);
 }
 
 function drawFace(ctx, radius) {
-  var grad;
-  ctx.beginPath();
-  ctx.arc(0, 0, radius, 0, 2*Math.PI);
-  ctx.fillStyle = 'white';
-  ctx.fill();
-  grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
-  grad.addColorStop(0, '#333');
-  grad.addColorStop(0.5, 'white');
-  grad.addColorStop(1, '#333');
-  ctx.strokeStyle = grad;
-  ctx.lineWidth = radius*0.1;
-  ctx.stroke();
-  ctx.beginPath();
-  ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
-  ctx.fillStyle = '#333';
-  ctx.fill();
+  canvas.style.background ="transparent";
 }
 
 function drawNumbers(ctx, radius) {
