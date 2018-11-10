a5d942634a1739df12e3e0c03542c1c753ea9d0f
tom mehrkam <tmehrkam@gmail.com>
2018-11-10 10:05:13 -0500

    Using CSS to build fonts

--- public/clock.js
+++ public/clock.js
@@ -2,41 +2,68 @@ var imgFontColor;
 var tz;
 updateCoords();  // grab map coords from backend.
 
-
-function updateCoords() {
-	url="coords";
-	var xhr = new XMLHttpRequest();  // need a sync call to initialize Maps
-	xhr.open("GET",url,false);
-	xhr.send(null);
-	var obj = JSON.parse(xhr.responseText);
-	imgFontColor = obj.imgFontColor;
-	tz = obj.tz;
-}
-
-document.body.style.color = imgFontColor;
-
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var radius = canvas.height / 2;
-ctx.translate(radius, radius);
-radius = radius * 0.90
-drawNumbers(ctx, radius);
-
-function drawNumbers(ctx, radius) {
-	var ang;
-	var num;
-	ctx.font = radius*0.15 + "px arial";
-	ctx.fillStyle = imgFontColor;
-	ctx.textBaseline="middle";
-	ctx.textAlign="center";
-	for(num = 1; num < 13; num++){
-		ang = num * Math.PI / 6;
-		ctx.rotate(ang);
-		ctx.translate(0, -radius*0.85);
-		ctx.rotate(-ang);
-		ctx.fillText(num.toString(), 0, 0);
-		ctx.rotate(ang);
-		ctx.translate(0, radius*0.85);
-		ctx.rotate(-ang);
-	}
+/*
+ * Starts any clocks using the user's local time
+ * From: cssanimation.rocks/clocks
+ */
+ initLocalClocks();
+ 
+ var imgFontColor;
+ var tz;
+ updateCoords();  // grab map coords from backend.
+ 
+ var hrDiv = document.getElementsByClassName('hours')[0];
+ var minDiv = document.getElementsByClassName('minutes')[0];
+ var secDiv = document.getElementsByClassName('seconds')[0];
+ 
+ hrDiv.style.backgroundColor = imgFontColor;
+ minDiv.style.backgroundColor = imgFontColor;
+ secDiv.style.backgroundColor = imgFontColor;
+ 
+ function updateCoords() {
+ 	url="coords";
+ 	var xhr = new XMLHttpRequest();  // need a sync call to initialize Maps
+ 	xhr.open("GET",url,false);
+ 	xhr.send(null);
+ 	var obj = JSON.parse(xhr.responseText);
+  	imgFontColor = obj.imgFontColor;
+ 	tz = obj.tz;
+ }
+ 
+function initLocalClocks() {	
+  var now = new Date().toLocaleString("en-US", {timeZone: tz});
+  console.log(now);
+  var date = new Date(now);
+  console.log(date);
+	
+  var seconds = date.getSeconds();
+  var minutes = date.getMinutes();
+  var hours = date.getHours();
+  // Create an object with each hand and it's angle in degrees
+  var hands = [
+    {
+      hand: 'hours',
+      angle: (hours * 30) + (minutes / 2)
+    },
+    {
+      hand: 'minutes',
+      angle: (minutes * 6)
+    },
+    {
+      hand: 'seconds',
+      angle: (seconds * 6)
+    }
+  ];
+  // Loop through each of these hands to set their angle
+  for (var j = 0; j < hands.length; j++) {
+    var elements = document.querySelectorAll('.' + hands[j].hand);
+    for (var k = 0; k < elements.length; k++) {
+        elements[k].style.webkitTransform = 'rotateZ('+ hands[j].angle +'deg)';
+        elements[k].style.transform = 'rotateZ('+ hands[j].angle +'deg)';
+        // If this is a minute hand, note the seconds position (to calculate minute position later)
+        if (hands[j].hand === 'minutes') {
+          elements[k].parentNode.setAttribute('data-second-angle', hands[j + 1].angle);
+        }
+    }
+  }
 }
