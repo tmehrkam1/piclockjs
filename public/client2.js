60c24e2d4c554f776cc464c7bf2d0e7682a54d72
Merky1 <merky1@users.noreply.github.com>
2019-02-16 19:41:16 -0500

    Update client2.js
    Messing with overlay array..

--- public/client2.js
+++ public/client2.js
@@ -173,9 +173,12 @@ function initMap() {
 	function updateRadar(){
 		
 		console.log("update tile # " + tileIndex);
-		map.overlayMapTypes.setAt(tileIndex,tileAeris);
+		map.overlayMapTypes.insertAt(6,tileAeris);
+		map.overlayMapTypes.removeAt(0);
 	
-		mapLocal.overlayMapTypes.setAt(tileIndex,tileAeris);
+		mapLocal.overlayMapTypes.insertAt(6,tileAeris);
+		mapLocal.overlayMapTypes.removeAt(0);
+	
 		tileIndex++;
 		if (tileIndex > 5) {
 			tileIndex=0;
