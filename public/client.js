f2c0d49591944be8a9d63c5079a1cedf7b926fd0
tom mehrkam <tmehrkam@gmail.com>
2019-03-22 16:13:37 -0400

    removing extra Aeris map code

--- public/client.js
+++ public/client.js
@@ -148,15 +148,6 @@ function initMap() {
 		var diffM = Math.round(((diffMs % 86400000) % 3600000) / 60000);
 		
 		if (diffM >= 5) {
-			tileAeris = new google.maps.ImageMapType({
-				getTileUrl: function(tile, zoom) {
-					return "https://maps.aerisapi.com/"+aerisID+"_"+aerisSecret+"/radar/"+zoom+"/"+tile.x+"/"+tile.y+"/current.png?bogus="+Date(); 
-				},
-				tileSize: new google.maps.Size(256, 256),
-				opacity:0.60,
-				name : 'current' + now,
-				isPng: true
-			});
 			
 			console.log("update tile # " + tileIndex);
 			map.overlayMapTypes.setAt(tileIndex,null);
