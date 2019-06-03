8cb72f3452c8bc724d713ff634a0cdb577b0b0fa
tom mehrkam <tmehrkam@gmail.com>
2019-06-03 13:07:29 -0400

    Added moon image detail page

---
+++ public/moon.js
@@ -0,0 +1,145 @@
+var lat;
+var lon;
+var gMapKey;
+var tz;
+var tileIndex = 0;
+
+updateCoords();  // grab map coords from backend.
+
+function updateCoords() {
+	url="coords";
+	var xhr = new XMLHttpRequest();  // need a sync call to initialize Maps
+	xhr.open("GET",url,false);
+	xhr.send(null);
+	var obj = JSON.parse(xhr.responseText);
+	lat = obj.lat;
+	lon = obj.lon;
+	lat = parseFloat(lat);
+	lon = parseFloat(lon);
+	gMapKey = obj.gMapKey;
+	clockType = obj.clock;
+	backgroundImg = obj.backgroundImg;
+	imgFontColor = obj.imgFontColor;
+	tz = obj.tz;
+	aerisID = obj.aerisID;
+	aerisSecret = obj.aerisSecret;
+}
+
+//restyle column width
+var col2Div = document.getElementById("col_2");
+var dateDiv = document.getElementById("date");
+var timeDiv = document.getElementById("time");
+
+col2Div.style.width = "79vw"; 
+dateDiv.style.float = "left";
+dateDiv.style.fontFamily = "orbitron";
+dateDiv.style.padding = "5px 0px 0px 0px";
+timeDiv.style.height = "10vh";
+timeDiv.style.fontSize = "7vh";
+timeDiv.style.lineHeight = "7vh";
+timeDiv.style.overflow = "hidden";
+timeDiv.style.float = "left";
+timeDiv.style.padding = "5px 0px 0px 50px";
+
+
+
+updateClock();
+updateCur();
+
+setInterval(updateClock, 1000); // tick the clock every second
+setInterval(updateCur, 10000); // every ten seconds update current conditions
+// from cache
+
+function updateClock() {
+	// update date string
+	var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', timeZone: tz };
+	var date = new Intl.DateTimeFormat('en-us',options).format(timeStamp);
+	document.getElementById("date").textContent = date;
+
+		var timeStamp = new Date();
+		var time = new Date().toLocaleTimeString("en-us", {
+			hour : '2-digit',
+			minute : '2-digit',
+			hour12 : 'true',
+			timeZone : tz
+		});
+		document.getElementById("time").textContent = time;
+}
+
+function updateCur() {
+	url="current";
+	fetch(url)
+	.then((resp) => resp.json())
+	.then(function(data){
+		var sunrise = new Date(data.sunrise).toLocaleTimeString("en-us",{
+			hour : '2-digit',
+			minute : '2-digit',
+			hour12 : 'true',
+			timeZone : tz
+		});
+		var sunset = new Date(data.sunset).toLocaleTimeString("en-us",{
+			hour : '2-digit',
+			minute : '2-digit',
+			hour12 : 'true',
+			timeZone : tz
+		});
+
+		if (data.pressureTrend == 1 || data.pressureTrend == null) {
+			presTrendIcon = '<i class="fas fa-circle"></i>';
+		} else if (data.pressureTrend < 1) {
+			presTrendIcon = '<i class="fas fa-chevron-down"></i>';
+		} else {
+			presTrendIcon = '<i class="fas fa-chevron-up"></i>';
+		}
+		document.getElementById("curIcon").innerHTML = data.curIcon;
+		document.getElementById("curHum").innerHTML = 'Humidity : ' + parseInt(data.humidity) + ' %';
+		document.getElementById("curTemp").innerHTML = data.tempF + ' &deg;F';
+		document.getElementById("curDesc").innerHTML = data.curDesc;
+		document.getElementById("curPres").innerHTML = 'pressure ' + data.pressure + ' mbar ' + presTrendIcon;
+		document.getElementById("curWind").innerHTML = 'wind ' + data.windSpeed + ' mph from ' + data.windDir;
+		updateBackground(data.tempF.toString());
+
+		if (data.feelsLike != null) {
+			document.getElementById("curFeels").innerHTML = 'Feels like ' + data.feelsLike + ' &deg;F';
+		} else {
+			document.getElementById("curFeels").innerHTML = null;
+		}
+
+	})
+	.catch(function(error){
+		alert(error);
+	})
+}
+
+// change background color based on temp
+function updateBackground(temp) {
+	if (temp < 30 ){
+		document.body.style.backgroundColor = "#94b7cf";
+		document.body.style.color = "#fce8dd";
+	} else if (temp>=90) {
+		document.body.style.backgroundColor = "#E91E24";
+		document.body.style.color = "#ffffff";
+	} else if (temp>=30 && temp<40){
+		document.body.style.backgroundColor = "#00A4E8";
+		document.body.style.color = "#ffe3df";
+	} else if (temp>=40 && temp<50){
+		document.body.style.backgroundColor = "#2D3389";
+		document.body.style.color = "#ffe3cc";
+	} else if(temp>=50 && temp<60){
+		document.body.style.backgroundColor = "#128A43";
+		document.body.style.color = "#8a1e12";
+	} else if (temp>=60 && temp<70){
+		document.body.style.backgroundColor = "#76BD43";
+		document.body.style.color = "#002B49";
+	}else if (temp>=70 && temp<80){
+		document.body.style.backgroundColor = "#FBD905";
+		document.body.style.color = '#002B49';
+	} else if (temp>=80 && temp<90){
+		document.body.style.backgroundColor = "#F58322";
+		document.body.style.color = '#002B49';
+	}
+}
+
+function reloadMain() {
+	window.location = "/";
+}
