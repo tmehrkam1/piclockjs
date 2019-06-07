34459d926b176b09aafbffcdbc8aba9a354aaa6b
tom mehrkam <tmehrkam@gmail.com>
2019-06-07 14:19:42 -0400

    skeleton for forecast details

--- public/forecast.js
+++ public/forecast.js
@@ -1,14 +1,20 @@
-
 var lat;
 var lon;
 var gMapKey;
-var clockType;
-var nightMode;
-var backgroundImg;
-var imgFontColor;
 var tz;
+var moonTime = new Date();
 
 updateCoords();  // grab map coords from backend.
+
+//shift clock into day mode
+url="http://127.0.0.1:8081/day";
+var xhttp = new XMLHttpRequest();
+xhttp.open("GET", url, true);
+xhttp.onerror = function () {
+	console.log("no response on 127.0.0.1:8081");
+};
+xhttp.send();
+
 
 function updateCoords() {
 	url="coords";
@@ -25,212 +31,39 @@ function updateCoords() {
 	backgroundImg = obj.backgroundImg;
 	imgFontColor = obj.imgFontColor;
 	tz = obj.tz;
+	aerisID = obj.aerisID;
+	aerisSecret = obj.aerisSecret;
 }
 
-//used to load the script and variablize the mapkey
-var addScript = document.createElement("script");
-addScript.type = "text/javascript";
-addScript.src = "https://maps.googleapis.com/maps/api/js?key=" + gMapKey + "&callback=initMap";
-addScript.async = true;
-addScript.defer = true;
-(document.getElementsByTagName("head")[0] || document.documentElement ).appendChild(addScript);
+//restyle column width
+var col2Div = document.getElementById("col_2");
+var dateDiv = document.getElementById("date");
+var timeDiv = document.getElementById("time");
+var moonImg = document.getElementById("moonimg");
+var moonDiv = document.getElementById("moon");
 
-function initMap() {
-
-	map = new google.maps.Map(document.getElementById('rdrRegional'), {
-		center: {lat: lat, lng: lon},
-		zoom: 7,
-		zoomControl: false,
-		mapTypeControl: false,
-		scaleControl: false,
-		streetViewControl: false,
-		rotateControl: false,
-		fullscreenControl: false,
-		gestureHandling: 'none',
-		mapTypeId: 'hybrid'
-	});
-	mapLocal = new google.maps.Map(document.getElementById('rdrLocal'), {
-		center: {lat: lat, lng: lon},
-		zoom: 11,
-		zoomControl: false,
-		mapTypeControl: false,
-		scaleControl: false,
-		streetViewControl: false,
-		rotateControl: false,
-		fullscreenControl: false,
-		gestureHandling: 'none',
-		mapTypeId: 'hybrid'
-	});
-
-	
-    var marker = new google.maps.Marker({
-        position: {lat: lat, lng: lon},
-        map: mapLocal,
-        title: 'map center'
-      });
-
-	tileAeris = new google.maps.ImageMapType({
-		getTileUrl: function(tile, zoom) {
-			return "https://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/nexrad-n0q-900913/" + zoom + "/" + tile.x + "/" + tile.y +".png?"+ (new Date()).getTime(); 
-		},
-		tileSize: new google.maps.Size(256, 256),
-		opacity:0.60,
-		name : 'current',
-		isPng: true
-	});
-
-	tileAeris5 = new google.maps.ImageMapType({
-		getTileUrl: function(tile, zoom) {
-			return "https://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/nexrad-n0q-900913-m05m/" + zoom + "/" + tile.x + "/" + tile.y +".png?"+ (new Date()).getTime();  
-		},
-		tileSize: new google.maps.Size(256, 256),
-		opacity:0,
-		name : '-5min',
-		isPng: true
-	});
-
-	tileAeris10 = new google.maps.ImageMapType({
-		getTileUrl: function(tile, zoom) {
-			return "https://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/nexrad-n0q-900913-m10m/" + zoom + "/" + tile.x + "/" + tile.y +".png?"+ (new Date()).getTime();  
-		},
-		tileSize: new google.maps.Size(256, 256),
-		opacity:0,
-		name : '-10min',
-		isPng: true
-	});
-
-	tileAeris15 = new google.maps.ImageMapType({
-		getTileUrl: function(tile, zoom) {
-			return "https://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/nexrad-n0q-900913-m15m/" + zoom + "/" + tile.x + "/" + tile.y +".png?"+ (new Date()).getTime(); 
-		},
-		tileSize: new google.maps.Size(256, 256),
-		opacity:0,
-		name : '-15min',
-		isPng: true
-	});
-
-	tileAeris20 = new google.maps.ImageMapType({
-		getTileUrl: function(tile, zoom) {
-			return "https://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/nexrad-n0q-900913-m20m/" + zoom + "/" + tile.x + "/" + tile.y +".png?"+ (new Date()).getTime();  
-		},
-		tileSize: new google.maps.Size(256, 256),
-		opacity:0,
-		name : '-20min',
-		isPng: true
-	});
-
-	tilePrecip = new google.maps.ImageMapType({
-		getTileUrl: function(tile, zoom) {
-			return "https://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/q2-n1p-900913/" + zoom + "/" + tile.x + "/" + tile.y +".png?"+ (new Date()).getTime(); 
-		},
-		tileSize: new google.maps.Size(256, 256),
-		opacity:0,
-		name : '-25min',
-		isPng: true
-	});
-
-	var radarFrame = 0;
-	var timeStamp = new Date();
-	var tileIndex =0;
-
-	console.log("loading radar");
-	map.overlayMapTypes.setAt("0",tileAeris20);
-	map.overlayMapTypes.setAt("1",tileAeris15);
-	map.overlayMapTypes.setAt("2",tileAeris10);
-	map.overlayMapTypes.setAt("3",tileAeris5);
-	map.overlayMapTypes.setAt("4",tileAeris);
-
-	mapLocal.overlayMapTypes.setAt("0",tileAeris20);
-	mapLocal.overlayMapTypes.setAt("1",tileAeris15);
-	mapLocal.overlayMapTypes.setAt("2",tileAeris10);
-	mapLocal.overlayMapTypes.setAt("3",tileAeris5);
-	mapLocal.overlayMapTypes.setAt("4",tileAeris);
-	mapLocal.overlayMapTypes.setAt("5",tilePrecip);
-
-	// setInterval(updateRadar(), 10000); // update radar loop every 5 minutes
-
-	timerId = window.setInterval(function () {
-		var now = new Date();
-		var diffMs = now - timeStamp;
-		var diffM = Math.round(((diffMs % 86400000) % 3600000) / 60000);
-
-		if (diffM >= 5) {
-
-			tileAeris = new google.maps.ImageMapType({
-				getTileUrl: function(tile, zoom) {
-					return "https://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/nexrad-n0q-900913/" + zoom + "/" + tile.x + "/" + tile.y +".png?"+ (new Date()).getTime(); 
-				},
-				tileSize: new google.maps.Size(256, 256),
-				opacity:0.60,
-				name : 'current',
-				isPng: true
-			});
-
-			tilePrecip = new google.maps.ImageMapType({
-				getTileUrl: function(tile, zoom) {
-					return "https://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/q2-n1p-900913/" + zoom + "/" + tile.x + "/" + tile.y +".png?"+ (new Date()).getTime(); 
-				},
-				tileSize: new google.maps.Size(256, 256),
-				opacity:0,
-				name : '-25min',
-				isPng: true
-			});
-
-			console.log("update tile # " + tileIndex);
-			map.overlayMapTypes.setAt(tileIndex,null);
-			map.overlayMapTypes.setAt(tileIndex,tileAeris);
-
-			mapLocal.overlayMapTypes.setAt(tileIndex,null);
-			mapLocal.overlayMapTypes.setAt(tileIndex,tileAeris);
-
-			mapLocal.overlayMapTypes.setAt(5,null);
-			mapLocal.overlayMapTypes.setAt(5,tilePrecip);
-
-			tileIndex++;
-			timeStamp = now;
-			console.log("tileIndex : " + tileIndex);
-			if (tileIndex >= 5) {
-				tileIndex=0;
-			}
-
-		}
-		for (i = 0;i < 5;i++) {
-			if (i == radarFrame) {
-				map.overlayMapTypes.getAt(i).setOpacity(.6);
-			} else {
-				map.overlayMapTypes.getAt(i).setOpacity(0);
-			}
-		}
-		// console.log("Animation frame : " + radarFrame);
-
-		radarFrame++;
-
-		if (radarFrame >= 4) {
-			radarFrame = 0;
-		} 
-	}, 1000);
-}
-
-
-if (backgroundImg !="") {
-	mainDiv = document.getElementById("main");
-	mainDiv.style.backgroundImage = "url("+backgroundImg+")";
-	mainDiv.style.backgroundSize ="cover";
-	mainDiv.style.color = imgFontColor;
-}
+col2Div.style.width = "79vw"; 
+dateDiv.style.float = "left";
+dateDiv.style.fontFamily = "orbitron";
+dateDiv.style.padding = "5px 0px 0px 0px";
+timeDiv.style.height = "10vh";
+timeDiv.style.fontSize = "7vh";
+timeDiv.style.lineHeight = "7vh";
+timeDiv.style.overflow = "hidden";
+timeDiv.style.float = "left";
+timeDiv.style.padding = "5px 0px 0px 50px";
+moonImg.style.height = "90vh";
+moonImg.style.width = "79vw";
+moonImg.style.objectFit = "contain";
 
 updateClock();
 updateCur();
 updateForecast();
-updateAlerts();
 
-if (clockType=="digital") { setInterval(updateClock, 1000)}; // tick the
-//clock every
-//second
+setInterval(updateClock, 1000); // tick the clock every second
 setInterval(updateCur, 10000); // every ten seconds update current conditions
-//from cache
-setInterval(updateForecast, 600000) // update the forecast every 10 min
-setInterval(updateAlerts,60000);  // update alerts every minute
+setInterval(updateForecast,4*60*60*1000) //update forcast block every 4 hours
+// from cache
 
 function updateClock() {
 	// update date string
@@ -238,8 +71,6 @@ function updateClock() {
 	var date = new Intl.DateTimeFormat('en-us',options).format(timeStamp);
 	document.getElementById("date").textContent = date;
 
-	// depending on analog vs digital, update clock
-	if (clockType=="digital") {
 		var timeStamp = new Date();
 		var time = new Date().toLocaleTimeString("en-us", {
 			hour : '2-digit',
@@ -248,18 +79,16 @@ function updateClock() {
 			timeZone : tz
 		});
 		document.getElementById("time").textContent = time;
-	} else {
-		// insert analog script here. Thinking I might move that off of
-		// index.html for sanity
-		var clock = document.createElement('iframe');
-		clock.src = "clock.html";
-		clock.setAttribute("id","analogClock");
-		clock.setAttribute("scrolling","no");
-		document.getElementById("time").appendChild(clock);
-	}
 
-
-
+		//update moon image every 4 hours
+		var diffMs = timeStamp - moonTime;
+		var diffM = Math.round(((diffMs % 86400000) % 3600000) / 60000 );
+		var diffH = parseInt(diffM / 60);
+		
+		if (diffH == 4) {
+			moonTime = new Date();
+			moonImg.src = "https://api.usno.navy.mil/imagery/moon.png?&ID=AA-URL";
+		}
 }
 
 function updateCur() {
@@ -293,7 +122,6 @@ function updateCur() {
 		document.getElementById("curDesc").innerHTML = data.curDesc;
 		document.getElementById("curPres").innerHTML = 'pressure ' + data.pressure + ' mbar ' + presTrendIcon;
 		document.getElementById("curWind").innerHTML = 'wind ' + data.windSpeed + ' mph from ' + data.windDir;
-		document.getElementById("sun_moon").innerHTML = 'Sunrise : ' + sunrise + '  Sunset : ' + sunset + '   Moon Phase : ' + data.moonPhase;
 		updateBackground(data.tempF.toString());
 
 		if (data.feelsLike != null) {
@@ -306,94 +134,10 @@ function updateCur() {
 	.catch(function(error){
 		alert(error);
 	})
+	updateAlerts();
 }
 
-function updateForecast() {
-	// setup a container for the entire forcast contents
-	var content = document.createElement("div");
-
-	url="forecast";
-	fetch(url)
-	.then((resp) => resp.json())
-	.then(function(data){
-		if (typeof data.list ==="undefined") {
-			data.list = [];
-		}
-		for (var i=0;i < data.list.length;i++) {		
-			// create a container for the forecast
-			var forecastBlock = document.createElement("div");
-			forecastBlock.setAttribute("class","forecastBlock");
-			forecastBlock.setAttribute("id","block"+i);
-
-			// create the image container
-			var forecastImage = document.createElement("div");
-			forecastImage.setAttribute("class","forecastImage");
-			forecastImage.setAttribute("id","imgDiv"+i);
-			if (nightMode == true) {
-				forecastImage.style.opacity = '.5';
-			}
-
-			// create the text container
-			var forecastText = document.createElement("div");
-			forecastText.setAttribute("class","forecastText");
-			forecastText.setAttribute("id","forecast"+i);
-
-			// populate the forecast icon with the image
-			var image = document.createElement("img");
-			image.setAttribute("src",data.list[i].icon);
-			image.setAttribute("style","height:100%;");
-
-			// populate the forecast text
-			forecastText.innerHTML=data.list[i].name + '<br />' + data.list[i].temp + '<br />' + data.list[i].short;
-
-			// put the image in the div
-			forecastImage.appendChild(image);
-
-			// put the image + text into the block
-			forecastBlock.appendChild(forecastImage);
-			forecastBlock.appendChild(forecastText);
-
-			// put the block into the parent div
-			content.appendChild(forecastBlock);
-
-		};
-		// put populated block into the column
-		document.getElementById("col_3").innerHTML = "";
-		document.getElementById("col_3").appendChild(content);
-	})
-	.catch(function(error){
-		alert(error);
-	});
-
-};
-
-function updateAlerts(){
-	var alertDiv = document.getElementById("alerts");
-	alertDiv.textContent="";
-
-	url="alerts";
-	fetch(url)
-	.then((resp) => resp.json())
-	.then(function(data){
-		for (var i=0;i < data.features.length;i++) {		
-			// create a container for the forecast
-			var alertBlock = document.createElement("div");
-			alertBlock.setAttribute("class","alertBlock");
-			alertBlock.setAttribute("id","alert"+i);
-
-			// populate the forecast text
-			alertBlock.innerHTML=data.features[i].headline;
-
-			// put the block into the parent div
-			alertDiv.appendChild(alertBlock);				
-		};
-
-	})
-	.catch(function(error){
-		alert(error);
-	});
-}
-//change background color based on temp
+// change background color based on temp
 function updateBackground(temp) {
 	if (temp < 30 ){
 		document.body.style.backgroundColor = "#94b7cf";
@@ -422,93 +166,85 @@ function updateBackground(temp) {
 	}
 }
 
-function toggleNight(){
-	var mainDiv = document.getElementById("main");
-	var radarDiv = document.getElementById("rdrStack");
-	var iconDiv = document.getElementById("curIcon");
-	var col3Div = document.getElementById("col_3");
-	var col2Div = document.getElementById("col_2");
-	var timeDiv = document.getElementById("time");
+function reloadMain() {
+	window.location = "/";
+}
 
-	if (nightMode == true) {
-		nightMode = false;
+function updateForecast() {
+	// setup a container for the entire forcast contents
+	var content = document.createElement("div");
 
-		mainDiv.style.backgroundColor = '';
-		if (backgroundImg == "") {
-			mainDiv.style.color = '';
-		} else {
-			mainDiv.style.backgroundImage = "url("+backgroundImg+")";
-			mainDiv.style.backgroundSize ="cover";
-			mainDiv.style.color = imgFontColor;
+	url="forecast";
+	fetch(url)
+	.then((resp) => resp.json())
+	.then(function(data){
+		if (typeof data.list ==="undefined") {
+			data.list = [];
 		}
+		// create a container for the forecast
+		var forecastBlock = document.createElement("div");
+		forecastBlock.setAttribute("class","forecastBlock");
+		forecastBlock.setAttribute("id","block");
+		
+		var forecastImage = document.createElement("div");
+		forecastImage.setAttribute("class","forecastImage");
+		forecastImage.setAttribute("id","imgDiv");
+		
+		// populate the forecast icon with the image
+		var image = document.createElement("img");
+		image.setAttribute("src",data.list[0].icon);
+		image.setAttribute("style","height:100%;");
+				
+		var forecastText = document.createElement("div");
+		forecastText.setAttribute("class","forecastText");
+		forecastText.setAttribute("id","forecast");
+		
+		// populate the forecast text
+		forecastText.innerHTML=data.list[0].name + '<br />' + '<span id="curTemp" >' + data.list[0].temp + ' &deg;F</span><br />' + data.list[0].detailed;
+		
+		// put the image in the div
+		forecastImage.appendChild(image);
 
+		// put the image + text into the block
+		forecastBlock.appendChild(forecastImage);
+		forecastBlock.appendChild(forecastText);
 
-		radarDiv.style.opacity = '1';
-		iconDiv.style.opacity = '1';
-		col3Div.style.visibility = "";
-		col2Div.style.width = "48vw";
-		timeDiv.style.fontSize  = "15vh";
+		// put the block into the parent div
+		content.appendChild(forecastBlock);
+		
+		// put populated block into the column
+		document.getElementById("curForecast").innerHTML = "";
+		document.getElementById("curForecast").appendChild(content);
+	})
+	.catch(function(error){
+		alert(error);
+	});
 
-		url="http://127.0.0.1:8081/day";
-		var xhttp = new XMLHttpRequest();
-		xhttp.open("GET", url, true);
-		xhttp.onerror = function () {
-			console.log("no response on 127.0.0.1:8081");
+}
+
+function updateAlerts(){
+	var alertDiv = document.getElementById("alerts");
+	alertDiv.textContent="";
+
+	url="alerts";
+	fetch(url)
+	.then((resp) => resp.json())
+	.then(function(data){
+		for (var i=0;i < data.features.length;i++) {		
+			// create a container for the forecast
+			var alertBlock = document.createElement("div");
+			alertBlock.setAttribute("class","alertBlock");
+			alertBlock.setAttribute("id","alert"+i);
+
+			// populate the forecast text
+			alertBlock.innerHTML=data.features[i].headline;
+
+			// put the block into the parent div
+			alertDiv.appendChild(alertBlock);				
 		};
-		xhttp.send();
 
-
-	} else {
-		nightMode = true;
-
-		mainDiv.style.backgroundColor = 'black';
-		mainDiv.style.backgroundImage ='';
-		mainDiv.style.color = 'darkgray';
-
-		radarDiv.style.opacity ='.5';
-		iconDiv.style.opacity = '.5';
-		col3Div.style.visibility = "hidden";
-		col2Div.style.width = "79vw";
-		timeDiv.style.fontSize  = "27vh";
-
-		url="http://127.0.0.1:8081/night";
-		var xhttp = new XMLHttpRequest();
-		xhttp.open("GET", url, true);
-		xhttp.onerror = function () {
-			console.log("no response on 127.0.0.1:8081");
-		};
-		xhttp.send();
-	}
-
+	})
+	.catch(function(error){
+		alert(error);
+	});
 }
-
-function radarPage(e) {
-	if (!e)
-	      e = window.event;
-
-	    //IE9 & Other Browsers
-	    if (e.stopPropagation) {
-	      e.stopPropagation();
-	    }
-	    //IE8 and Lower
-	    else {
-	      e.cancelBubble = true;
-	    }
-	    window.location = "/radar.html"
-}
-
-function moonPage(e) {
-	if (!e)
-	      e = window.event;
-
-	    //IE9 & Other Browsers
-	    if (e.stopPropagation) {
-	      e.stopPropagation();
-	    }
-	    //IE8 and Lower
-	    else {
-	      e.cancelBubble = true;
-	    }
-	    window.location = "/moon.html"
-}
-
