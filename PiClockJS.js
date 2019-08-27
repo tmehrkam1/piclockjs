f9d2a8a1e1a4b506c6d77eafc48f2de7d620b7a4
tom mehrkam <tmehrkam@gmail.com>
2019-08-27 16:11:01 -0400

    not maximizing properly

--- PiClockJS.js
+++ PiClockJS.js
@@ -201,11 +201,11 @@ if (settings.mode == "local" || settings.mode == "client") {
 
 	function createWindow () {
 		// Create the browser window.
-		win = new BrowserWindow({frame: false,webPreferences: {nodeIntegration: false}})
+		win = new BrowserWindow({frame: false,webPreferences: {nodeIntegration: false}});
 
 		// and load the index.html of the app.
-		win.loadURL(settings.servIP)
-		win.maximize()
+		win.loadURL(settings.servIP);
+		win.maximize();
 	}
 
 	app.on('ready', createWindow)	
