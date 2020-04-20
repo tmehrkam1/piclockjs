6ec7d93651f41bd1c572a134be92a4c00e24463f
tom mehrkam <tmehrkam@gmail.com>
2020-04-20 16:27:36 -0400

    adding api test script

---
+++ api-test.js
@@ -0,0 +1,188 @@
+/**
+ * replacement for piclock using node and html tmehrkam@gmail.com
+ */
+var http = require('http');
+var request = require('request');
+var fs = require("fs");
+var path = require('path');
+var d2d = require('degrees-to-direction')
+var util = require('util');
+var trend = require('trend');
+var getPromise = util.promisify(request.get);
+var DOMParser = require('xmldom').DOMParser;
+var geoTz = require('geo-tz');
+var SunCalc = require('suncalc');
+const { exec } = require('child_process');
+
+//Read settings
+const settings = JSON.parse(fs.readFileSync('./settings.json'))
+
+//get current weather conditions
+var cur={};
+//initialize json of arrays
+var store={};
+store.timestamp=[];
+store.temp=[];
+store.pressure=[];
+store.humidity=[];
+var forecasts = {};
+var alerts = {};
+var nightMode = false;
+
+cur.dt=0;
+alerts.features =[];
+
+
+//Logging
+var winston = require('winston');
+require('winston-daily-rotate-file');
+const NODE_ENV = process.env.NODE_ENV;
+const myFormat = winston.format.printf(info => {
+	return `${info.timestamp} ${info.level}: ${info.message}`;
+});
+
+
+var transport = new (winston.transports.DailyRotateFile)({
+  filename: 'apitest-%DATE%.log',
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '14d'
+});
+
+transport.on('rotate', function(oldFilename, newFilename) {
+  // do something fun
+});
+
+const logger = winston.createLogger({
+	level: NODE_ENV === "production" ? 'warn' : 'info',
+			transports: [
+			      transport
+			    ],
+			 format: winston.format.combine(
+				winston.format.timestamp({
+					format: 'YYYY-MM-DD hh:mm:ss A ZZ'
+				}),
+				winston.format.colorize({ all: true }),
+				winston.format.simple(),
+				myFormat
+		),
+});
+
+async function currentOwObs(){
+	var url = 'http://api.openweathermap.org/data/2.5/weather?lat='+settings.lat+'&lon='+settings.lon+'&appid='+settings.owAppId+'&units=imperial'
+	logger.info(url);
+
+	var { body } = await getPromise({
+		url: url,
+		json: true,
+		headers: {'User-Agent': 'piclockjs'}
+	});
+	parseOW(body);
+	getWgovObs(settings.wgStaID);
+}
+
+async function currentDsObs(){
+	var url = 'https://api.darksky.net/forecast/'+settings.dsAppId+'/'+settings.lat+','+settings.lon;
+	logger.info(url);
+
+	var { body } = await getPromise({
+		url: url,
+		json: true,
+		headers: {'User-Agent': 'piclockjs'}
+	});
+	parseDS(body);
+}
+
+function parseOW(observation){
+	var now = new Date();
+
+	if (observation.dt <= cur.dt)
+	{
+		var update = new Date(0);
+		var current = new Date(0);
+
+		update.setUTCSeconds(observation.dt);
+		current.setUTCSeconds(cur.dt);
+
+		var diffMs = (now - update); // diff in MS
+		var diffMins = Math.round(diffMs / 1000 / 60); // minutes
+
+		var diffCur = (current - update);
+		var diffCurMins = (diffCur / 1000 / 60);
+
+		logger.info('stale update detected with timestamp : ' + update + " behind current timestamp by : " + diffCurMins + " behind now by : "+ diffMins + " minutes");
+		return;
+	}
+
+	var sunriseEpoch = new Date(0);
+	var sunsetEpoch = new Date(0);
+
+
+
+	sunriseEpoch.setUTCSeconds(observation.sys.sunrise);
+	sunsetEpoch.setUTCSeconds(observation.sys.sunset);
+
+	if ((now > sunsetEpoch ) || (now < sunriseEpoch)) {
+		cur.curIcon = '<i class="wi wi-owm-night-' + observation.weather[0].id +'"></i>';
+	} else {
+		cur.curIcon = '<i class="wi wi-owm-day-' + observation.weather[0].id +'"></i>';
+	}
+
+	cur.tempF = observation.main.temp;
+	cur.pressure = observation.main.pressure;
+	cur.humidity = observation.main.humidity;
+	cur.windSpeed = observation.wind.speed;
+	cur.windDir = d2d(observation.wind.deg);
+	cur.curDesc = observation.weather[0].main;
+	cur.sunrise = sunriseEpoch.toString();
+	cur.sunset = sunsetEpoch.toString();
+	cur.dt = observation.dt;
+	
+	storeValues(cur.dt,cur.tempF,cur.pressure,cur.humidity);
+}
+
+function parseDS(body){
+	
+	var observation = body.currently;
+	var now = new Date();
+
+	if (observation.time <= cur.dt)
+	{
+		var update = new Date(0);
+		var current = new Date(0);
+
+		update.setUTCSeconds(observation.time);
+		current.setUTCSeconds(cur.dt);
+
+		var diffMs = (now - update); // diff in MS
+		var diffMins = Math.round(diffMs / 1000 / 60); // minutes
+
+		var diffCur = (current - update);
+		var diffCurMins = (diffCur / 1000 / 60);
+
+		logger.info('stale update detected with timestamp : ' + update + " behind current timestamp by : " + diffCurMins + " behind now by : "+ diffMins + " minutes");
+		return;
+	}
+	
+	var sunriseEpoch = new Date(0);
+	var sunsetEpoch = new Date(0);
+
+	sunriseEpoch.setUTCSeconds(body.daily.data[0].sunriseTime);
+	sunsetEpoch.setUTCSeconds(body.daily.data[0].sunsetTime);
+	cur.sunrise = sunriseEpoch.toString();
+	cur.sunset = sunsetEpoch.toString();
+
+	cur.curIcon = '<i class="wi wi-forecast-io-' + observation.icon +'"></i>';
+
+	cur.tempF = Math.round(parseFloat(observation.temperature));
+	cur.pressure = Math.round(parseFloat(observation.pressure));
+	cur.humidity = Math.round(parseFloat(observation.humidity * 100));
+	cur.windSpeed = observation.windSpeed;
+	cur.windDir = d2d(observation.windBearing);
+	cur.curDesc = observation.summary;
+	cur.dt = observation.time;
+	cur.feelsLike = Math.round(parseFloat(observation.apparentTemperature));
+	
+	storeValues(cur.dt,cur.tempF,cur.pressure,cur.humidity);
+}
