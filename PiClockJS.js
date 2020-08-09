015180f94830a05895de716ef7fd8f003c09cec6
tom mehrkam <tmehrkam@gmail.com>
2020-08-09 15:14:34 -0400

    adding a retry to the forecast pull

--- PiClockJS.js
+++ PiClockJS.js
@@ -338,6 +338,9 @@ async function wgForecast(url){
 	}
 	catch(e) {
 		logger.error(e);
+		var now = new Date();
+		timer.fore = now - (settings.forecastInterval * 1000) + (60 * 1000);
+		logger.warn("set next forecast poll to : " + timer.fore)
 	}
 }
 
