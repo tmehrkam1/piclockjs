cd3c8fdc3743e509b18b5982c6dfe21b9b797d83
Merky1 <merky1@users.noreply.github.com>
2020-09-10 22:32:10 -0400

    attempting to subtract the polling interval

--- PiClockJS.js
+++ PiClockJS.js
@@ -364,7 +364,7 @@ async function wgForecast(url){
 	var now = new Date();
     if (url == null) {
     		logger.warn("forecast gridpoint data not updated");
-		timer.fore = now - settings.forecastInterval * 1000 + 60 * 1000;
+		timer.fore = now - (settings.forecastInterval * 1000);
 		logger.warn("set next forecast poll to : " + Date(timer.fore));
 		return;
     }
