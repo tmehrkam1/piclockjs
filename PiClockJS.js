641551d138f621d8c58cca47133ea679ea7e1d18
tom mehrkam <tmehrkam@gmail.com>
2020-09-13 10:55:09 -0400

    using math.abs

--- PiClockJS.js
+++ PiClockJS.js
@@ -417,7 +417,7 @@ async function wgCurrent(staId) {
 	var now = new Date();
     if (typeof staId === 'undefined') {
     	logger.warn("current gridpoint data not updated");
-		timer.cur = (now.getUTCMilliseconds() - settings.currentConditionsInterval * 1000) + (60 * 1000);
+		timer.cur = (now - settings.currentConditionsInterval * 1000) + (60 * 1000);
 		logger.warn("set next current poll to : " + timer.cur);
 		return;
     }
