cf02c49b5dfe10eb5da2370f930f88f9dc6420e4
Merky1 <merky1@users.noreply.github.com>
2020-12-27 17:28:32 -0500

    forgot to comment out example

--- PiClockJS.js
+++ PiClockJS.js
@@ -669,7 +669,7 @@ function parsewgCurrent(data) {
 		logger.warn('stale update detected with timestamp : ' + update + " behind current timestamp by : " + diffCurMins + " behind now by : "+ diffMins + " minutes");
 		return;
 	} else if (observation.temperature.qualityControl == "qc:Z") {
-		PHKO 272053Z 25006KT 10SM FEW030 27/17 A3006 RMK AO2 SLP179 T02720167 50005
+		//PHKO 272053Z 25006KT 10SM FEW030 27/17 A3006 RMK AO2 SLP179 T02720167 50005
 		var metar = observation.rawMessage;
 		var tempC = metar.search("/T(\d{4})/g");
 		if (tempC.match("/1\d{3}/g") then {
