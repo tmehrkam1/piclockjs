042e446c64188f9229ac1d627cfb76fcda6d8839
Merky1 <merky1@users.noreply.github.com>
2020-12-27 18:14:33 -0500

    added debug

--- PiClockJS.js
+++ PiClockJS.js
@@ -672,6 +672,7 @@ function parsewgCurrent(data) {
 		//PHKO 272053Z 25006KT 10SM FEW030 27/17 A3006 RMK AO2 SLP179 T02720167 50005
 		var metar = observation.rawMessage;
 		var temp = metar.match(/T(\d{4})/g);
+		logger.warn(temp);
 		tempC = temp[0];
 		if (tempC.match(/1\d{3}/g)) {
 		 tempC = -tempC.search(/d(\d{3})/g)/10;
