68d4a8dc616d8aba5198b63c6338258912be423e
Merky1 <merky1@users.noreply.github.com>
2021-01-05 18:32:21 -0500

    regex foo

--- PiClockJS.js
+++ PiClockJS.js
@@ -671,8 +671,8 @@ function parsewgCurrent(data) {
 	} else if (observation.temperature.qualityControl == "qc:Z") {
 		//PHKO 272053Z 25006KT 10SM FEW030 27/17 A3006 RMK AO2 SLP179 T02720167 50005
 		var metar = observation.rawMessage;
-		var temp = metar.match(/T(\d{4})/g);
-		tempC=temp[0].match(/(\d{4})/g);
+		var temp = metar.match(/^(T\d)(\d{3})/g);
+		
 		logger.warn(temp);
 		
 		if (tempC.match(/1\d{3}/g)) {
