48ed036f113de88c029d81a16b86f2c056d8712a
Merky1 <merky1@users.noreply.github.com>
2021-07-25 19:09:11 -0400

    js date math

--- public/client.js
+++ public/client.js
@@ -282,7 +282,7 @@ function updateCur() {
 			timeZone : tz
 		});
 		
-		var daylight = (sunset - sunrise);
+		var daylight = (sunset.getTime() - sunrise.getTime());
 
 		if (data.pressureTrend == 1 || data.pressureTrend == null) {
 			presTrendIcon = '<i class="fas fa-circle"></i>';
