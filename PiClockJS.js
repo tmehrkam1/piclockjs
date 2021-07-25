85184b524e7bc4845853aaa6c1846862b04e1cd7
Merky1 <merky1@users.noreply.github.com>
2021-07-25 17:16:18 -0400

    Fixed while loop to if

--- PiClockJS.js
+++ PiClockJS.js
@@ -727,24 +727,23 @@ function generateSunTimes(){
 function parseWgForecast(data) {
 	// usg forecast has a tendancy to mess up now()
 	var now = new Date();
+	var array = [];
 	
-	var array = []
 	for (var i =0; i < 9; i++) {
 		var end = new Date(data.properties.periods[i].endTime);
-		while ( end > now ){
-			var forecast ={};  // temp object to build json
+		if ( end > now ){
+			var forecast ={};  // temp object to build json	
 			forecast.name = data.properties.periods[i].name;
 			forecast.temp = data.properties.periods[i].temperature;
 			forecast.short = data.properties.periods[i].shortForecast;
 			forecast.icon = data.properties.periods[i].icon;
 			forecast.detailed = data.properties.periods[i].detailedForecast;
 			array.push(forecast);
+		} else {
+			data.properties.periods.shift();
+			logger.warn("WG forecast array shifted")
+			continue;
 		}
-	} else {
-		data.properties.periods.shift();
-		logger.warn("WG forecast array shifted")
-		continue;
-	}
 		
 	forecasts.list = array;
 }
