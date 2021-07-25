349a03db9b7e0adfe492f22cc79b1a20029b9840
Merky1 <merky1@users.noreply.github.com>
2021-07-25 19:07:20 -0400

    removed math

--- public/client.js
+++ public/client.js
@@ -282,7 +282,7 @@ function updateCur() {
 			timeZone : tz
 		});
 		
-		var daylight = (sunset - sunrise) / 1000 / 60 / 60;
+		var daylight = (sunset - sunrise);
 
 		if (data.pressureTrend == 1 || data.pressureTrend == null) {
 			presTrendIcon = '<i class="fas fa-circle"></i>';
