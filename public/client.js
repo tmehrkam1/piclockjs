7b1d4abdb4875f3d8a7104986d1b2594fbc889d8
Merky1 <merky1@users.noreply.github.com>
2021-07-25 20:05:23 -0400

    Capitalized daylight

--- public/client.js
+++ public/client.js
@@ -302,7 +302,7 @@ function updateCur() {
 		document.getElementById("curDesc").innerHTML = data.curDesc;
 		document.getElementById("curPres").innerHTML = 'pressure ' + data.pressure + ' mbar ' + presTrendIcon;
 		document.getElementById("curWind").innerHTML = 'wind ' + data.windSpeed + ' mph from ' + data.windDir;
-		document.getElementById("sun_moon").innerHTML = 'Sunrise : ' + sunrise + '  Sunset : ' + sunset + '   Moon Phase : ' + data.moonPhase +"<p> daylight: " + dlhrs + " hours " + dlmin + " minutes";
+		document.getElementById("sun_moon").innerHTML = 'Sunrise : ' + sunrise + '  Sunset : ' + sunset + '   Moon Phase : ' + data.moonPhase +"<p> Daylight: " + dlhrs + " hours " + dlmin + " minutes";
 		
 		document.body.style.backgroundColor = data.bg;
 		document.body.style.color = data.color;
