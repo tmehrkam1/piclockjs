29b760a168617be41ca6b0cef9de25bb5a61f149
Merky1 <merky1@users.noreply.github.com>
2022-02-18 17:06:26 -0500

    switching to different clock

--- public/client.js
+++ public/client.js
@@ -250,6 +250,22 @@ function updateClock() {
 		//document.getElementById("analogClock").hidden = true;
 		var timearr	 = time.split(" ");
 		document.getElementById("time").textContent = timearr[0];
+	} else {
+	setInterval(() => {
+	d = new Date(); //object of date()
+	hr = d.getHours();
+	min = d.getMinutes();
+	sec = d.getSeconds();
+	hr_rotation = 30 * hr + min / 2; //converting current time
+	min_rotation = 6 * min;
+	sec_rotation = 6 * sec;
+
+	hour.style.transform = `rotate(${hr_rotation}deg)`;
+	minute.style.transform = `rotate(${min_rotation}deg)`;
+	second.style.transform = `rotate(${sec_rotation}deg)`;
+}, 1000);
+
+
 	}
 }
 
