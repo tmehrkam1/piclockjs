ca6247b93fb9e155156818866487250792b670c4
merky1 <167227328+tmehrkam1@users.noreply.github.com>
2025-04-15 19:31:02 -0400

    hmmm

--- PiClockJS.js
+++ PiClockJS.js
@@ -482,7 +482,7 @@ async function wgCurrent(staId) {
 		return;
     }
     
-	var url = "https://api.weather.gov/stations/"+staId+"/observations/latest"+"?date="+now;
+	var url = "https://api.weather.gov/stations/"+staId+"/observations/latest?date="+Date.toString;
 	logger.info(url);
 
 	try {
